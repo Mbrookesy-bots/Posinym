745331c0d30fdbc1560cd17214ec24af00358ba8
Mbrookesy <56401421+Mbrookesy@users.noreply.github.com>
2020-08-21 15:04:11 +0100

    Adding authentication checks

---
+++ main/main.py
@@ -0,0 +1,23 @@
+import tweepy
+
+#Gather data from config file for authentication
+with open("../config.txt", "r") as config:
+
+    file = config.readlines()
+
+    consumerApiKey = file[0][18:44]
+    consumerApiSecretKey = file[2][25:76]
+    bearerToken = file[4][14:133]
+    authApiKey = file[6][19:69]
+    authApiSecret = file[8][26:72]
+
+    auth = tweepy.OAuthHandler(consumerApiKey, consumerApiSecretKey)
+    auth.set_access_token(authApiKey, authApiSecret)
+
+    api = tweepy.API(auth)
+
+    try:
+        api.verify_credentials()
+        print("Authentication OK")
+    except:
+        print("Error")
